28baa120041dc4510193c2103954c25a17a72483
Gabeg55 <gabeisgoody12@gmail.com>
2025-04-14 16:45:26 -0400

    Create upload.js

---
+++ pages/upload.js
@@ -0,0 +1,14 @@
+import Navbar from "../components/Navbar";
+import "../styles/globals.css";
+
+export default function Upload() {
+  return (
+    <>
+      <Navbar />
+      <main style={{ padding: "2rem", textAlign: "center" }}>
+        <h2>Upload Your Mechanics Video</h2>
+        <p>Feature coming soon. You'll be able to upload and analyze pitching mechanics using AI.</p>
+      </main>
+    </>
+  );
+}
