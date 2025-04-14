fa3ec117da68dc523ee0b5fac9aa3df83f2e16eb
Gabeg55 <gabeisgoody12@gmail.com>
2025-04-14 16:46:09 -0400

    Create plan.js

---
+++ pages/plan.js
@@ -0,0 +1,15 @@
+import Navbar from "../components/Navbar";
+import "../styles/globals.css";
+
+export default function Plan() {
+  return (
+    <>
+      <Navbar />
+      <main style={{ padding: "2rem", textAlign: "center" }}>
+        <h2>Create Your Custom Pitching Plan</h2>
+        <p>Answer a few questions and we’ll design a program built for your needs.</p>
+        <p>[Feature coming soon!]</p>
+      </main>
+    </>
+  );
+}
